23a51a2dd72aae408e79ceaa1d9f3528284196b5
Mica-schatz <Micaelaschatzz22@gmail.com>
2024-09-15 05:37:42 -0300

    Cambio de nombre de botones,labels, textos y clases

--- TemplateTPIntegrador/Datos/UTILS/LoginDatos.cs
+++ TemplateTPIntegrador/Datos/UTILS/LoginDatos.cs
@@ -4,8 +4,20 @@
     {
         public bool ExisteUsuario(string usuario, string password)
         {
-            // Simulación de verificación de usuario
-            return usuario == "admin" && password == "1234";
+            // Simulación de verificación de múltiples usuarios
+            if ((usuario == "admin" && password == "1234") ||
+                (usuario == "lean" && password == "4321") ||
+                (usuario == "mica" && password == "5678") ||
+                (usuario == "juli" && password == "8765") ||
+                (usuario == "flor" && password == "1357") ||
+                (usuario == "marti" && password == "2468"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
--- TemplateTPIntegrador/TemplateTPIntegrador/Utils/ValidacionesUtils.cs
+++ TemplateTPIntegrador/TemplateTPIntegrador/Utils/ValidacionesUtils.cs
@@ -1,6 +1,6 @@
 ﻿using System.Windows.Forms;
 
-namespace TemplateTPIntegrador.UTILS
+namespace PRESENTACION
 {
     public class ValidacionesYUtiles
     {
