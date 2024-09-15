8559a81b54f4ef1cdb1749b153a1d8f17b67f9dd
Mica-schatz <Micaelaschatzz22@gmail.com>
2024-09-15 05:17:22 -0300

    Modificacion de archivos y mas usuarios agregados

--- TemplateTPIntegrador/TemplateTPIntegrador/Program.cs
+++ TemplateTPIntegrador/TemplateTPIntegrador/Program.cs
@@ -1,22 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace PRESENTACION
 {
     internal static class Program
     {
-        /// <summary>
-        /// Punto de entrada principal para la aplicación.
-        /// </summary>
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frm_inicio());
+            Application.Run(new frm_login()); // Asegúrate de que esté referenciado el formulario correcto
         }
     }
 }
---
+++ TemplateTPIntegrador/TemplateTPIntegrador/frm_login.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Forms;
+using NEGOCIO;
+
+namespace PRESENTACION
+{
+    public partial class frm_login : Form
+    {
+        public frm_login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Validar campos vacíos
+            ValidacionesYUtiles validaciones = new ValidacionesYUtiles();
+            if (validaciones.ValidarStringVacio(txt_username, txt_password))
+            {
+                return;
+            }
+
+            // Validar login
+            LoginNegocio loginNegocio = new LoginNegocio();
+            if (loginNegocio.ValidarLogin(txt_username.Text, txt_password.Text))
+            {
+                MessageBox.Show("Login exitoso");
+                // Aquí puedes abrir otro formulario si es necesario
+            }
+            else
+            {
+                MessageBox.Show("Usuario o contraseña incorrectos");
+            }
+        }
+
+        private void frm_login_Load(object sender, EventArgs e)
+        {
+            // Código que se ejecute al cargar el formulario
+        }
+    }
+}
