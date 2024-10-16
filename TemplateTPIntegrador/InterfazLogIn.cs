6c34baaf15d14652efe8d59db26849ccb7209874
JulianDiaz <juli.diaz277@hotmail.com>
2024-10-15 22:43:11 -0300

    Login y Menu

---
+++ TemplateTPIntegrador/InterfazLogIn.cs
@@ -0,0 +1,414 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using Negocio;
+using System.Linq;
+using System.Collections.Generic;
+using Datos;
+
+namespace Presentacion
+{
+    public partial class InterfazLogIn : Ventana
+    {
+        public string Host { get; set; } // Variable miembro para almacenar el número de host del usuario
+
+        public string idVendedor { get; set; } //Variable para almacenar el idVendedor del usuario (nos va a servir para las ventas)
+
+        public static InterfazLogIn Instancia { get; private set; } // Retengo la instancia para manipular el Menú en función de quién se logueó.
+
+        // Definir las credenciales del administrador
+        private const string UsuarioDefault = "ADMINI24";
+        private const string ContraseñaDefault = "CAI20241";
+
+        public InterfazLogIn() 
+        {
+            InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen;
+            KeyPreview = true;
+            Instancia = this;
+        }
+
+        public void IniciarSesion()
+        {
+            UsuarioNegocio iniciarSesion = new UsuarioNegocio();
+
+            string id = iniciarSesion.IniciarSesion(Box_Usuario.Text, Box_Pass.Text); // Almacenar el hash del id de usuario
+            string Usuario = Box_Usuario.Text.ToUpper(); // Permitir que la persona ingrese minusculas en el campo "Nombre de usuario"
+            string Contraseña = Box_Pass.Text;
+            string errorUsuario = Validar.CampoEnBlanco(Usuario);
+            string errorContraseña = Validar.CampoEnBlanco(Contraseña);
+            idVendedor = id ; // Le asignamos al idVendedor el valor del id de inicio de sesión, lo vamos a utilizar para agregar la venta)
+
+            if (errorUsuario == "1") // Verificar si hay errores en el nombre de usuario y contraseña
+            {
+                MessageBox.Show("No se ingresó el usuario.", "Atención!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return; // Salir del método sin continuar con el inicio de sesión ni contar errores
+            }
+
+            if (errorContraseña == "1") // Verificar si hay errores en el nombre de usuario y contraseña
+            {
+                MessageBox.Show("No se ingresó la contraseña.", "Atención!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return; // Salir del método sin continuar con el inicio de sesión ni contar errores
+            }
+
+            if (Usuario == UsuarioDefault && Contraseña == ContraseñaDefault)
+            {
+                Host = Validar.NumeroHost(Usuario); // capturo el host para usarlo en el menú.
+                DialogResult = DialogResult.OK;
+                Close();
+                return; // Salir del método después de permitir el acceso al administrador
+            }
+
+            if (Validar.EsID(id.Substring(1, 36)) == 0)
+            {
+                MessageBox.Show("El usuario no existe en el servidor o\n\nEl servidor está caído.\n\nCódigo de Error: 101", "Fallo en la Conexión", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            if (Validar.EsID(id.Substring(1, 36)) == 2) //Hay que llamar a esto solamente cuando la contraseña está mal, pero el usuario existe
+            {
+                MessageBox.Show("Credenciales Incorrectas.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                CargarTXT(Usuario, id); 
+            }
+
+            if (Validar.EsID(id.Substring(1, 36)) == 1) // El id es válido, permitir el acceso
+            {
+                ResetearIntentosFallidos(Usuario); // volver el contador de errores a 0.
+                if (Usuario == Contraseña)
+                {
+                    Box_Usuario.Enabled = false;
+                    Box_Pass.Enabled = false;
+                    NewPass.Visible = true;
+                    NewPass.Enabled = true;
+                    ConfirmNewPass.Visible = true;
+                    ConfirmNewPass.Enabled = true;
+                    Label_NewPass.Visible = true;
+                    Label_NewPass.Enabled = true;
+                    Label_ConfirmNewPass.Visible = true;
+                    Label_ConfirmNewPass.Enabled = true;
+                    Boton_Ingresar.Visible = false;
+                    CambiarClave.Visible = true;
+                    PassViewImg.Enabled = false;
+
+                    MessageBox.Show("Por favor, ingrese una nueva contraseña", "Primer Ingreso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                }
+                else if(Validar.ContraseñaExpirada(Usuario))     //Valido si la contraseña expiró
+                {
+                    MessageBox.Show("La contraseña expiró. Debe cambiarla antes de continuar.", "Contraseña Expirada", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    Box_Usuario.Enabled = false;
+                    Box_Pass.Enabled = false;
+                    NewPass.Visible = true;
+                    NewPass.Enabled = true;
+                    ConfirmNewPass.Visible = true;
+                    ConfirmNewPass.Enabled = true;
+                    Label_NewPass.Visible = true;
+                    Label_NewPass.Enabled = true;
+                    Label_ConfirmNewPass.Visible = true;
+                    Label_ConfirmNewPass.Enabled = true;
+                    Boton_Ingresar.Visible = false;
+                    CambiarClave.Visible = true;
+                    PassViewImg.Enabled = false;
+                }
+                else if(Validar.UsuarioInactivo(Usuario)){
+                    MessageBox.Show("El usuario se encuentra inactivo. Contáctese con el administrador del sistema.", "Usuario Inactivo",MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    Host = Validar.NumeroHost(Usuario); // Guardar el número de host del usuario
+
+                    DialogResult = DialogResult.OK;
+                    Hide(); 
+                }
+            }
+        }
+
+        private void CargarTXT(string Usuario, string id)
+        {
+            UsuarioNegocio negocio = new UsuarioNegocio();
+            List<UsuariosActivos> listaUsuarios = negocio.ListarUsuarios();
+
+            // Filtrar usuarios cuyo NombreUsuario empiece por "G7"
+            listaUsuarios = listaUsuarios.Where(u => u.NombreUsuario.StartsWith("G7")).ToList();
+
+            UsuariosActivos usuarioEncontrado = listaUsuarios.Find(u => u.NombreUsuario == Usuario);
+
+            if (usuarioEncontrado != null)
+            {
+                string nombreArchivo = "Usuarios.txt"; //Cargo la ruta del TXT
+                string directorio = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "CAI", "Equipo1");
+                string path = Path.Combine(directorio, nombreArchivo);
+                try
+                {
+                    if (!Directory.Exists(directorio))  //Crea las carpetas si no existen
+                    {
+                        Directory.CreateDirectory(directorio);
+                    }
+                    if (!File.Exists(path))
+                    {
+                        File.WriteAllText(path, Usuario + ";1" + Environment.NewLine);
+                    }
+                    else
+                    {
+                        string[] lineasTXT = File.ReadAllLines(path);
+                        bool UsuarioExiste = lineasTXT.Any(linea => linea.StartsWith(Usuario + ";"));
+
+                        if (!UsuarioExiste)
+                        {
+                            using (StreamWriter writer = File.AppendText(path))
+                            {
+                                writer.WriteLine(Usuario + ";1;" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + ";activo" + Environment.NewLine); //Si llega acá es porque ya falló un intento.
+                            }
+                        }
+                        else
+                        {
+                            // Si existe tenemos que editarlo
+                            int indexUsuario = -1;
+                            for (int i = 0; i < lineasTXT.Length; i++) //Acá ubico el index en el array del usuario que está ingresando
+                            {
+                                if (lineasTXT[i].StartsWith(Usuario + ";"))
+                                {
+                                    indexUsuario = i;
+                                    break;
+                                }
+                            }
+
+                            if (indexUsuario != -1)
+                            {
+                                string[] parametros = lineasTXT[indexUsuario].Split(';');
+                                int intentos = int.Parse(parametros[1]);
+                                intentos++;
+                                // Mantener la fecha del último cambio de contraseña
+                                string fechaUltimoCambio = parametros.Length > 2 ? parametros[2] : ""; //Esto es un condicional. Si existe parametro 2 lo pone. si no existe pone vacío
+                                string estado = parametros.Length > 3 ? parametros[3] : "activo";
+
+                                lineasTXT[indexUsuario] = Usuario + ";" + intentos.ToString() + ";" + fechaUltimoCambio + ";" + estado;
+
+                                File.WriteAllLines(path, lineasTXT);
+
+                                if (intentos == 3)
+                                {
+                                    MessageBox.Show("Se han alcanzado los tres intentos permitidos para iniciar sesión.\n\nDe haber un intento fallido más, el usuario será bloqueado.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                                }
+                                else if (intentos >= 4)
+                                {
+                                    //UsuarioNegocio negocioUser = new UsuarioNegocio();    //El método borrar usuario no funciona. Lo reemplazamos por el .txt
+                                    //negocioUser.BorrarUsuario(id);
+                                    lineasTXT[indexUsuario] = Usuario + ";" + intentos.ToString() + ";" + fechaUltimoCambio + ";inactivo";
+                                    File.WriteAllLines(path, lineasTXT);
+                                    MessageBox.Show("El usuario ha sido bloqueado. Contacte al Administrador del Sistema para reactivarlo nuevamente", "Usuario Bloqueado", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                                    Application.Exit();
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (IOException ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+        }
+
+        private void ResetearIntentosFallidos(string Usuario)
+        {
+            string nombreArchivo = "Usuarios.txt";
+            string directorio = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "CAI", "Equipo1");
+            string path = Path.Combine(directorio, nombreArchivo);
+            try
+            {
+                if (!Directory.Exists(directorio))
+                {
+                    Directory.CreateDirectory(directorio);
+                }
+
+                if (!File.Exists(path))
+                {
+                    // Si el archivo no existe, lo creamos y escribimos la información del usuario actual
+                    File.WriteAllText(path, Usuario + ";0;" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + ";activo" + Environment.NewLine);
+                    SetearSession(Usuario);
+                }
+                else
+                {
+                    string[] lineasTXT = File.ReadAllLines(path);
+                    bool ExisteUsuario = false;
+
+                    for (int i = 0; i < lineasTXT.Length; i++)
+                    {
+                        if (lineasTXT[i].StartsWith(Usuario + ";"))
+                        {
+                            string[] parametros = lineasTXT[i].Split(';');
+                            string fechaUltimoCambio = parametros.Length > 2 ? parametros[2] : "";
+                            string estado = parametros.Length > 3 ? parametros[3] : "activo";
+
+                            lineasTXT[i] = Usuario + ";" + "0" + ";" + fechaUltimoCambio + ";" + estado; //Actualizo el valor de intentos fallidos a 0
+
+                            SetearSession(Usuario);
+                            ExisteUsuario = true;
+                            break;
+                        }
+                    }
+
+                    if (!ExisteUsuario)
+                    {
+                        string[] nuevaLinea = { Usuario + ";0;" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + ";activo" + Environment.NewLine };
+                        List<string> lineasLista = lineasTXT.ToList();
+                        lineasLista.AddRange(nuevaLinea);
+                        lineasTXT = lineasLista.ToArray();
+
+                        SetearSession(Usuario);
+                    }
+
+                    File.WriteAllLines(path, lineasTXT);
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        } 
+
+        private void SetearSession(string NombreUsuario)
+        {
+            UsuarioNegocio negocio = new UsuarioNegocio();
+            List<UsuariosActivos> listaUsuarios = negocio.ListarUsuarios();
+            listaUsuarios = listaUsuarios.Where(u => u.NombreUsuario.StartsWith("G7")).ToList();
+            UsuariosActivos usuario = listaUsuarios.FirstOrDefault(u => u.NombreUsuario == NombreUsuario);
+
+            if (usuario != null)
+            {
+                //Tag = new SessionData { Usuario = NombreUsuario, Host = usuario.Host};
+            }
+        }
+
+        private void Boton_Ingresar_Click(object sender, EventArgs e)
+        {
+            IniciarSesion();
+        }
+
+        private void CambiarClave_Click(object sender, EventArgs e)
+        {
+            string usuario = Box_Usuario.Text;
+            string contraseña = Box_Pass.Text;
+            string nuevaContraseña = NewPass.Text;
+            string confirmacionContraseña = ConfirmNewPass.Text;
+
+            string errorNuevaContraseña = Validar.ConfirmarContraseña(nuevaContraseña, confirmacionContraseña);
+            if (errorNuevaContraseña != null)
+            {
+                MessageBox.Show(errorNuevaContraseña, "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                errorNuevaContraseña = Validar.EsContraseña(nuevaContraseña);
+                if (errorNuevaContraseña != null)
+                {
+                    MessageBox.Show(errorNuevaContraseña, "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else if (contraseña == nuevaContraseña)
+                {
+                    MessageBox.Show("La contraseña no puede ser igual a la actual.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else
+                {
+                    try
+                    {
+                        UsuarioNegocio negocio = new UsuarioNegocio();
+                        negocio.CambiarContraseña(usuario, contraseña, nuevaContraseña);
+                        Host = Validar.NumeroHost(usuario);
+                        MessageBox.Show("Contraseña actualizada con éxito!", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        ActualizarFechaTXT(usuario);
+                        DialogResult = DialogResult.OK;
+                        Hide();
+                    }
+                    catch (IOException ex)
+                    {
+                        MessageBox.Show("No se pudo cambiar la Contraseña: " + ex.Message, "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+
+                }
+            }
+        }
+
+        private void Boton_Cancelar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PassViewImg_MouseDown(object sender, MouseEventArgs e) // Muestra la contraseña al hacer click en el ojo
+        {
+            Box_Pass.UseSystemPasswordChar = false; 
+        }
+
+        private void PassViewImg_MouseUp(object sender, MouseEventArgs e)
+        {
+            Box_Pass.UseSystemPasswordChar = true; 
+        } // Oculta la contraseña al hacer click en el ojo
+
+        private void LogIn_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                DialogResult resultado = MessageBox.Show("¿Desea salir del programa?\n\nSe cerrará la ventana y no se guardarán los datos.", "Confirmar Cancelación", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (resultado == DialogResult.Yes)
+                {
+                    Application.Exit();
+                }
+            }
+        }  // Evento KeyDown para cerrar la ventana con la tecla ESC
+
+        private void ActualizarFechaTXT(string usuario) //Acá va a cargar la fecha de cambio de contraseña
+        {
+            string nombreArchivo = "Usuarios.txt";
+            string directorio = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "CAI", "Equipo1");
+            string path = Path.Combine(directorio, nombreArchivo);
+
+            if (!Directory.Exists(directorio))
+            {
+                Directory.CreateDirectory(directorio);
+            }
+            if (!File.Exists(path))
+            {
+                File.WriteAllText(path, "");
+            }
+            string[] lineasTXT = File.ReadAllLines(path);
+            bool usuarioEncontrado = false;
+            for (int i = 0; i < lineasTXT.Length; i++)
+            {
+                if (lineasTXT[i].StartsWith(usuario + ";"))
+                {
+                    string[] parametros = lineasTXT[i].Split(';');
+
+                    lineasTXT[i] = usuario + ";" + parametros[1] + ";" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss" + ";activo");
+                    usuarioEncontrado = true;
+                    File.WriteAllLines(path, lineasTXT);
+                    break;
+                }
+            }
+            if (!usuarioEncontrado)
+            {
+                using (StreamWriter writer = File.AppendText(path))
+                {
+                    writer.WriteLine(usuario + ";0;" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + ";activo");
+                }
+            }
+
+            File.WriteAllLines(path, lineasTXT);
+        }
+
+        private void LogoInicio_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PassViewImg_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
+
+
+
---
+++ TemplateTPIntegrador/InterfazMenu.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Presentacion
+{
+    partial class InterfazMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UsuariosTexto = new System.Windows.Forms.Label();
+            this.ProveedoresTexto = new System.Windows.Forms.Label();
+            this.ClientesTexto = new System.Windows.Forms.Label();
+            this.ProductosTexto = new System.Windows.Forms.Label();
+            this.ReportesTexto = new System.Windows.Forms.Label();
+            this.VentasTexto = new System.Windows.Forms.Label();
+            this.BotonSalir = new System.Windows.Forms.Button();
+            this.ProductosIcono = new System.Windows.Forms.PictureBox();
+            this.VentasIcono = new System.Windows.Forms.PictureBox();
+            this.ReportesIcono = new System.Windows.Forms.PictureBox();
+            this.ClientesIcono = new System.Windows.Forms.PictureBox();
+            this.ProveedoresIcono = new System.Windows.Forms.PictureBox();
+            this.UsuariosIcono = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ProductosIcono)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.VentasIcono)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ReportesIcono)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ClientesIcono)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ProveedoresIcono)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.UsuariosIcono)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // UsuariosTexto
+            // 
+            this.UsuariosTexto.AutoSize = true;
+            this.UsuariosTexto.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.UsuariosTexto.Font = new System.Drawing.Font("Nunito", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UsuariosTexto.Location = new System.Drawing.Point(109, 284);
+            this.UsuariosTexto.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.UsuariosTexto.Name = "UsuariosTexto";
+            this.UsuariosTexto.Size = new System.Drawing.Size(150, 43);
+            this.UsuariosTexto.TabIndex = 0;
+            this.UsuariosTexto.Text = "Usuarios";
+            this.UsuariosTexto.Click += new System.EventHandler(this.UsuariosIcono_Click);
+            
+            // BotonSalir
+            // 
+            this.BotonSalir.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.BotonSalir.FlatAppearance.BorderColor = System.Drawing.Color.Gainsboro;
+            this.BotonSalir.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Khaki;
+            this.BotonSalir.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gold;
+            this.BotonSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BotonSalir.Font = new System.Drawing.Font("Nunito", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BotonSalir.Location = new System.Drawing.Point(1134, 685);
+            this.BotonSalir.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.BotonSalir.Name = "BotonSalir";
+            this.BotonSalir.Size = new System.Drawing.Size(183, 49);
+            this.BotonSalir.TabIndex = 6;
+            this.BotonSalir.Text = "Salir";
+            this.BotonSalir.UseVisualStyleBackColor = false;
+            this.BotonSalir.Click += new System.EventHandler(this.BotonSalir_Click);
+            
+            // 
+            // UsuariosIcono
+            // 
+            this.UsuariosIcono.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.UsuariosIcono.Image = global::Presentacion.Properties.Resources.grupo;
+            this.UsuariosIcono.Location = new System.Drawing.Point(54, 81);
+            this.UsuariosIcono.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.UsuariosIcono.Name = "UsuariosIcono";
+            this.UsuariosIcono.Size = new System.Drawing.Size(260, 200);
+            this.UsuariosIcono.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.UsuariosIcono.TabIndex = 7;
+            this.UsuariosIcono.TabStop = false;
+            this.UsuariosIcono.Click += new System.EventHandler(this.UsuariosIcono_Click);
+            // 
+            // InterfazMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1366, 768);
+            this.Controls.Add(this.ProductosIcono);
+            this.Controls.Add(this.VentasIcono);
+            this.Controls.Add(this.ReportesIcono);
+            this.Controls.Add(this.ClientesIcono);
+            this.Controls.Add(this.ProveedoresIcono);
+            this.Controls.Add(this.UsuariosIcono);
+            this.Controls.Add(this.VentasTexto);
+            this.Controls.Add(this.ReportesTexto);
+            this.Controls.Add(this.ProductosTexto);
+            this.Controls.Add(this.ClientesTexto);
+            this.Controls.Add(this.ProveedoresTexto);
+            this.Controls.Add(this.UsuariosTexto);
+            this.Controls.Add(this.BotonSalir);
+            this.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
+            this.Name = "InterfazMenu";
+            this.Text = "Menu";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Menu_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.ProductosIcono)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.VentasIcono)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ReportesIcono)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ClientesIcono)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ProveedoresIcono)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.UsuariosIcono)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label UsuariosTexto;
+        private System.Windows.Forms.Label ProveedoresTexto;
+        private System.Windows.Forms.Label ClientesTexto;
+        private System.Windows.Forms.Label ProductosTexto;
+        private System.Windows.Forms.Label ReportesTexto;
+        private System.Windows.Forms.Label VentasTexto;
+        private System.Windows.Forms.Button BotonSalir;
+        private System.Windows.Forms.PictureBox UsuariosIcono;
+        private System.Windows.Forms.PictureBox ProveedoresIcono;
+        private System.Windows.Forms.PictureBox ClientesIcono;
+        private System.Windows.Forms.PictureBox ReportesIcono;
+        private System.Windows.Forms.PictureBox VentasIcono;
+        private System.Windows.Forms.PictureBox ProductosIcono;
+    }
+}
