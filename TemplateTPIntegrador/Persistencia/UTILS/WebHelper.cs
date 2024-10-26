01b03f7ad319c38295bd8d5d738aad82cf370b35
Leandro Drabenche <leandro.d.drabenche@accenture.com>
2024-10-26 11:15:33 -0300

    Persistancia y Rebase

---
+++ TemplateTPIntegrador/Persistencia/UTILS/WebHelper.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+
+namespace Persistencia
+{
+    public class WebHelper
+    {
+        static HttpClient HttpClient = new HttpClient();
+        static String RutaBase = "https://cai-tp.azurewebsites.net";
+
+        public static HttpResponseMessage Get(string url)
+        {
+            var uri = RutaBase + url;
+
+            HttpResponseMessage response = HttpClient.GetAsync(uri).Result;  // Blocking call!
+
+            return response;
+        }
+
+        public static HttpResponseMessage Post(string url, string jsonRequest)
+        {
+            var uri = RutaBase + url;
+
+            var data = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = HttpClient.PostAsync(uri, data).Result;
+
+            return response;
+        }
+
+        public static HttpResponseMessage Put(string url, string jsonRequest)
+        {
+            var uri = RutaBase + url;
+
+            var data = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = HttpClient.PutAsync(uri, data).Result;
+
+            return response;
+        }
+
+        public static HttpResponseMessage Patch(string path, string jsonRequest)
+        {
+            var uri = RutaBase + path;
+
+            var data = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
+            var request =
+                new HttpRequestMessage(new HttpMethod("PATCH"), uri)
+                {
+                    Content = data
+                };
+
+            HttpResponseMessage response = HttpClient.SendAsync(request).Result;
+
+            return response;
+        }
+
+        public static HttpResponseMessage Delete(string url)
+        {
+            var uri = RutaBase + url;
+
+            HttpResponseMessage response = HttpClient.DeleteAsync(uri).Result;
+
+            return response;
+        }
+
+        public static HttpResponseMessage DeleteWithBody(string url, string jsonRequest)
+        {
+            var uri = RutaBase + url;
+
+            HttpRequestMessage request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Delete,
+                RequestUri = new Uri(uri),
+                Content = new StringContent(jsonRequest, Encoding.UTF8, "application/json")
+            };
+            HttpResponseMessage response = HttpClient.SendAsync(request).Result;
+
+            return response;
+        }
+    }
+}
+
---
+++ TemplateTPIntegrador/Persistencia/UsuarioService.cs
@@ -0,0 +1,195 @@
+﻿using Datos;
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+
+namespace Persistencia
+{
+    public class UsuarioService
+    {
+        public List<UsuariosActivos> GetUsuarios()
+        {
+            String path = "/api/Usuario/TraerUsuariosActivos?id=70b37dc1-8fde-4840-be47-9ababd0ee7e5";
+            List<UsuariosActivos> Usuarios = new List<UsuariosActivos>();
+
+            try
+            {
+                HttpResponseMessage response = WebHelper.Get(path);
+                if (response.IsSuccessStatusCode)
+                {
+                    var contentStream = response.Content.ReadAsStringAsync().Result;
+                    List<UsuariosActivos> listadoUsuarios = JsonConvert.DeserializeObject<List<UsuariosActivos>>(contentStream);
+                    return listadoUsuarios;
+                }
+                else
+                {
+                    Console.WriteLine($"Error: {response.StatusCode} - {response.ReasonPhrase}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+            }
+            return Usuarios;
+        }
+
+        public void ModificarUsuario(Guid idUsuario, String direccion, String telefono, String email)
+        {
+            String path = "/api/Usuario/PatchUsuario";
+            Dictionary<string, string> map = new Dictionary<string, string>
+            {
+                { "id", idUsuario.ToString() },
+                { "direccion", direccion },
+                { "telefono", telefono },
+                { "email", email }
+            };
+
+            var jsonRequest = JsonConvert.SerializeObject(map);
+
+            try
+            {
+                HttpResponseMessage response = WebHelper.Patch(path, jsonRequest);
+                if (response.IsSuccessStatusCode)
+                {
+                    var reader = new StreamReader(response.Content.ReadAsStreamAsync().Result);
+                    string respuesta = reader.ReadToEnd();
+                }
+                else
+                {
+                    Console.WriteLine($"Error: {response.StatusCode} - {response.ReasonPhrase}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+            }
+        }
+
+        public void AgregarUsuario(AltaUsuario AltaUsuario)
+        {
+            String path = "/api/Usuario/AgregarUsuario";
+
+            var jsonRequest = JsonConvert.SerializeObject(AltaUsuario);
+
+            try
+            {
+                HttpResponseMessage response = WebHelper.Post(path, jsonRequest);
+                if (response.IsSuccessStatusCode)
+                {
+                    var reader = new StreamReader(response.Content.ReadAsStreamAsync().Result);
+                    string respuesta = reader.ReadToEnd();
+                }
+                else
+                {
+                    var reader = new StreamReader(response.Content.ReadAsStreamAsync().Result);
+                    string respuesta = reader.ReadToEnd();
+                    Console.WriteLine($"Error: {response.StatusCode} - {response.ReasonPhrase}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+            }
+        }
+
+        public void BorrarUsuario(string idUsuario) //Validar Tema de Parametro
+        {
+
+            Dictionary<string, string> map = new Dictionary<string, string>();
+            map.Add("id", idUsuario.ToString());
+            map.Add("idUsuario", "70b37dc1-8fde-4840-be47-9ababd0ee7e5");
+
+            var jsonRequest = JsonConvert.SerializeObject(map);
+
+            String path = "/api/Usuario/BajaUsuario";
+
+            try
+            {
+                HttpResponseMessage response = WebHelper.DeleteWithBody(path, jsonRequest);
+                if (response.IsSuccessStatusCode)
+                {
+                    var reader = new StreamReader(response.Content.ReadAsStreamAsync().Result);
+                    string respuesta = reader.ReadToEnd();
+                }
+                else
+                {
+                    Console.WriteLine($"Error: {response.StatusCode} - {response.ReasonPhrase}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+            }
+        }
+
+        public string IniciarSesion(LogIn IniciarSesion)
+        {
+            string id = null; // Variable para almacenar el id
+
+            string path = "/api/Usuario/Login";
+            var jsonRequest = JsonConvert.SerializeObject(IniciarSesion);
+
+            try
+            {
+                HttpResponseMessage response = WebHelper.Post(path, jsonRequest);
+                if (response.IsSuccessStatusCode)
+                {
+                    var contentStream = response.Content.ReadAsStreamAsync().Result;
+                    using (var reader = new StreamReader(contentStream))
+                    {
+                        string responseContent = reader.ReadToEnd();
+                        id = responseContent; // Asignar el id del body
+                        Console.WriteLine($"Hash: {id}");
+                    }
+                }
+                else
+                {
+                    var contentStream = response.Content.ReadAsStreamAsync().Result;
+                    using (var reader = new StreamReader(contentStream))
+                    {
+                        string responseContent = reader.ReadToEnd();
+                        id = responseContent; // Asignar el id del body
+                        Console.WriteLine($"Error: {response.StatusCode} - {response.ReasonPhrase}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+            }
+
+            return id; // Retornar el id obtenido
+        }
+        public void CambiarContraseña(string nombreUsuario, string contraseña, string contraseñaNueva)
+        {
+            string path = "/api/Usuario/CambiarContraseña";
+            Dictionary<string, string> map = new Dictionary<string, string>();
+            map.Add("nombreUsuario", nombreUsuario);
+            map.Add("contraseña", contraseña);
+            map.Add("contraseñaNueva", contraseñaNueva);
+            var jsonRequest = JsonConvert.SerializeObject(map);
+            try
+            {
+                HttpResponseMessage response = WebHelper.Patch(path, jsonRequest);
+                if (response.IsSuccessStatusCode)
+                {
+                    var reader = new StreamReader(response.Content.ReadAsStreamAsync().Result);
+                    string respuesta = reader.ReadToEnd();
+                }
+                else
+                {
+                    Console.WriteLine($"Error: {response.StatusCode} - {response.ReasonPhrase}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+            }
+        }
+
+    }
+}
+
